71e0152442dc2a4bdb54ddc61c0e082361a81058
Jorge-coder02 <jorgitenis@hotmail.es>
2025-03-04 17:41:41 +0100

    Guardado con DynamicWord

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Asegúrate de que este contenido esté ahí
+  ],
+  plugins: [
+    require("daisyui"), // Agregar DaisyUI como plugin
+  ],
+};
