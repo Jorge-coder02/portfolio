5f9cc3a553b19a34cb5fb7524d77639f101c1f76
Jorge-coder02 <jorgitenis@hotmail.es>
2025-03-05 20:33:30 +0100

    Resuelto problema build import css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
+// module.exports = {
+//   content: [
+//     "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Asegúrate de que este contenido esté ahí
+//   ],
+//   plugins: [
+//     require("daisyui"), // Agregar DaisyUI como plugin
+//   ],
+// };
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Asegúrate de que este contenido esté ahí
-  ],
-  plugins: [
-    require("daisyui"), // Agregar DaisyUI como plugin
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
 };
